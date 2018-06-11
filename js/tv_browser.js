86157995cdbf919d8951f2faf3968d767b324d72
Shaunprawn <shaun.seah@live.com.sg>
2018-06-11 10:05:51 +0800

    incomplete homework

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -1,2 +1,26 @@
 // API Docs at:
 // http://www.tvmaze.com/api
+
+function xhrActivate (){
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', responseHandler());
+	xhr.addEventListener('error', error());
+            xhr.open('GET', url);
+            xhr.send();
+        };
+
+function error () {
+	  console.log("error text", this.responseText);
+	  console.log("status text", this.statusText);
+	  console.log("status code", this.status);
+	};
+
+var responseHandler = function() {
+	  console.log("response text", this.responseText);
+	  console.log("status text", this.statusText);
+	  console.log("status code", this.status);
+	};
+
+
+
+var returnData = JSON.parse(this.responseText)
